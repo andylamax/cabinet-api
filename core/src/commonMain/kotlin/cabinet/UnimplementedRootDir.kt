df2b84e2d0b97e1888c818de2a0f3b55fcb8ec75
andylamax <andylamax@programmer.net>
2023-12-19 15:47:48 +0300

    Migrating to K2 and primitive collections

--- core/src/commonMain/kotlin/cabinet/UnimplementedRootDir.kt
+++ core/src/commonMain/kotlin/cabinet/UnimplementedRootDir.kt
@@ -2,14 +2,14 @@ package cabinet
 
 import kollections.List
 import kollections.Map
-import kollections.iMapOf
+import kollections.mapOf
 import koncurrent.Later
 import koncurrent.TODOLater
 
 class UnimplementedRootDir : RootDir {
     override fun upload(param: FileUploadParam): Later<Attachment> = TODOLater()
 
-    override fun upload(params: Array<FileUploadParam>): Map<FileUploadParam, Later<Attachment>> = iMapOf()
+    override fun upload(params: Array<FileUploadParam>): Map<FileUploadParam, Later<Attachment>> = mapOf()
 
     override fun list(): Later<List<Attachment>> = TODOLater()
 
